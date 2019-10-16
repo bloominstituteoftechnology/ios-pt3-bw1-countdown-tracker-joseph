7ef7a34e811e638fe10457aa5238395bc408c21e
Joe Rogers <48137568+JosephRogers94@users.noreply.github.com>
2019-10-16 17:47:29 -0500

    Add Base Xcode Project-Add .gitignore
    Added the Boilerplate code and project ignore files to begin working on.

---
+++ Countdown/Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Joseph Rogers on 10/16/19.
+//  Copyright © 2019 Moka Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
