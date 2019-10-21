f3095663450989135ff3da804601412b14abe440
Joseph Rogers <joerogersss2016@gmail.com>
2019-10-20 20:39:27 -0500

    Files added
    Files added for their respective Views

---
+++ Countdown/Countdown/View Controllers/CountdownTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountdownTableViewCell.swift
+//  Countdown
+//
+//  Created by Joseph Rogers on 10/20/19.
+//  Copyright © 2019 Moka Apps. All rights reserved.
+//
+
+import UIKit
+
+class CountdownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
